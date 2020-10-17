8272ac700f78cd00464265a86f50a98d1583a2e7
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2020-10-18 02:15:33 +0530

    week names using switch case

---
+++ Warm up/P01.cpp
@@ -0,0 +1,51 @@
+//Write a C++ program to print day of week name using switch case.
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int ch ;
+
+    cout<<" Enter the number from 1 to 7 of the week days:\n";
+    cin>>ch;
+
+    switch(ch)
+    {
+        case 1:
+            cout<<"Monday";
+            break;
+
+        case 2:
+            cout<<"Tuesday";
+            break;
+
+        case 3:
+            cout<<"Wednesday";
+            break;
+
+        case 4:
+            cout<<"Thursday";
+            break;
+
+        case 5:
+            cout<<"Friday";
+            break; 
+
+        case 6:
+            cout<<"Saturday";
+            break;
+
+        case 7:
+            cout<<"Sunday";
+            break;
+
+        default:
+            cout<<"Invalid Coice!";
+
+
+    }
+
+    return 0;
+
+}
