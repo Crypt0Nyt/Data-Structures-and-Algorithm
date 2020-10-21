dc1ec27b3a22d2bb2ddfdd29e20af2efda43a996
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2020-10-21 23:18:59 +0530

    Create Reverse of a string (GFG).c

---
+++ Arrays and Strings/Reverse of a string (GFG).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int T, i, len;
+    char S[100];
+    scanf("%d", &T);
+    getchar();
+    while (T--)
+    {
+        gets(S);
+        len = strlen(S);
+        for (i = len - 1; i >= 0; i--)
+        {
+            printf("%c", S[i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
