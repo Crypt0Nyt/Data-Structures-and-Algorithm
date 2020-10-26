87778a23e9b62467e3f8a716fbbe127e578bc737
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2020-10-27 00:57:46 +0530

    Create P04.cpp

---
+++ P04.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a  , b ;
+    cout<<"Enter the first Numbers:";
+    cin>>a;
+    cout<<"Enter the second Number:";
+    cin>>b;
+
+    switch(a < b)
+    {
+        case 0:
+            cout<<"Maximum number is:"<<a;
+            break;
+
+        case 1:
+            cout<<"Maximum number is:"<<b;
+            break;
+
+        default:
+            cout<<"Both are equal!!!";
+
+    }
+    return 0;
+}
