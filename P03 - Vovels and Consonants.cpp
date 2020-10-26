6558b07f5b2e7037cb0bef2109e86e97e436dc4c
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2020-10-26 21:55:07 +0530

    Create P03 - Vovels and Consonants.cpp

---
+++ P03 - Vovels and Consonants.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    char ch;
+
+    cout<<"Enter the character you want(a-z):";
+    cin>>ch;
+
+    switch(ch)
+    {
+        case 'a':
+            cout<<"vovel";
+            break;
+
+        case 'A':
+            cout<<"vovel";
+            break;
+
+        case 'e':
+            cout<<"vovel";
+            break;
+
+        case 'E':
+            cout<<"vovel";
+            break;
+
+        case 'o':
+            cout<<"vovel";
+            break;
+
+        case 'O':
+            cout<<"vovel";
+            break;
+
+        case 'u':
+            cout<<"vovel";
+            break;
+
+        case 'U':
+            cout<<"vovel";
+            break;
+
+        default:
+            cout<<"consonant";
+            break;
+
+
+    }
+
+    return 0;
+}
