e0584725d5a731de6f3b831319c80383675224b6
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2020-10-30 18:43:46 +0530

    Natural numbers in reverse order in while loop.cpp

---
+++ Programs/P10.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n ;
+
+    cout<<"Enter the number from which you want to print the Natural numbers in reverse order:";
+    cin>>n;
+
+    while(n >= 1)
+    {
+        cout<< n <<"  ";
+        n--;
+
+    }
+
+    return 0;
+}
