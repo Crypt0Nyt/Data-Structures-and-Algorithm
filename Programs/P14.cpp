4b67149ac11e7eec9dc28fb1deb3e8b2227fd131
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2020-11-02 23:30:52 +0530

    Natural Numbers from 1 to n

---
+++ Programs/P14.cpp
@@ -0,0 +1,20 @@
+//Write a program to find sum of all natural numbers between 1 to n.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i , sum = 0;
+
+    cout<<"Sum of all natural numbers from 1 to 100 are => ";
+
+    for(i=1 ; i <= 100 ; i++)
+    {
+        sum = sum + i;
+    }
+    cout<<sum;
+
+    return 0;
+}
+
+
