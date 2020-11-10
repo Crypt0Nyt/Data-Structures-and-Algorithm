e31de1d0ebec1a78b748c59f29f99fc28c499c18
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2020-11-10 23:33:00 +0530

    First and last digit

---
+++ Programs/P19.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int last, first, n;
+
+    cout<<"Enter the number=>";
+    cin>>n;
+
+    last = n % 10;
+
+    while(n >= 10){
+        n = n/10;
+    }
+
+    first = n;
+
+
+    cout<<"The First term is ="<<first<<endl<<"and the last term of the number is ="<<last;   
+    return 0;
+}
