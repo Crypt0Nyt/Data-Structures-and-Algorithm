999e16b7a8d68e7f12f2cde6bada6d67f79a7b4f
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2020-12-20 11:16:08 +0530

    GCD of two numbers

---
+++ Geeks-For-Geeks/G02.cpp
@@ -0,0 +1,27 @@
+//To find the GCD of two numbers
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int GCD(int a, int b)
+{
+    if(b==0) return a;
+    return GCD(b, a % b);
+}
+
+int main()
+{
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    
+     int T;
+        cin>>T;
+        while(T--)
+        {
+            int a, b;
+            cin>>a>>b;
+            cout<<GCD(a,b)<<endl;
+        }
+
+    return 0;
+}
